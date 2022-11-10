ccd2dd031ae5238f01e98c77ba789e1827811bcd
Marco Inchingoli <evilmarkus@hotmail.it>
2022-11-10 16:19:40 +0100

    Add First Component

---
+++ src/components/SiteHeader.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    name: 'SiteHeader'
+}
+
+</script>
+
+<template>
+    <header id="site_header">
+        <div class="navbar">
+            <img src="../assets/img/dc-logo.png" alt="">
+        </div>
+    </header>
+</template>
+
+<style>
+.navbar {
+    background-color: black;
+}
+</style>
